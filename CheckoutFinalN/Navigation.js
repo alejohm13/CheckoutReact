6c59725a7634ef85fd1554bbb62aea412b681a77
alejohm13 <152233449+alejohm13@users.noreply.github.com>
2024-04-12 03:09:18 -0500

    Add files via upload

---
+++ CheckoutFinalN/Navigation.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function Navigation() {
+    return (
+        <nav className="w-full h-14 hidden bg-white lg:flex p-4 justify-between">
+            <div className="h-auto w-auto">
+                <p className="text-xl font-black">STYLE SYNERGY</p>
+            </div>
+            <div className="flex space-x-8">
+                <a className="font-bold text-md" href="">MNG</a>
+                <a className="font-bold text-md" href="">AMERICAN BRANDS</a>
+                <a className="font-bold text-md" href="">RIFLE</a>
+                <a className="font-bold text-md" href="">NAF-NAF</a>
+                <input className="outline-none p-3 rounded-full shadow-lg" placeholder="busca tu producto" type="search" />
+            </div>
+            <div className="flex space-x-4">
+                <a href="">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                    </svg>
+                </a>
+                <a href="">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
+                </a>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+                </svg>
+            </div>
+        </nav>
+    );
+}
+
+export default Navigation;
---
+++ CheckoutFinalN/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from './Navigation';
+
+function App() {
+    return (
+        <div className="App">
+            <Navigation />
+            <h2>Gracias por su pago</h2>
+        </div>
+    );
+}
+
+export default App;
